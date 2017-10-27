c9c6acdfc049d6c3055fdef42417c339fe023364
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-10-26 19:54:18 -0500

    añade lo realizado en clase2

--- app/index.js
+++ app/index.js
@@ -2,14 +2,36 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import './styles/base.css'
+// no se puede renderizra objetos como hijos
 
-const App = () => (
-  <div className="title">
-    EC React - Semana 2: React Components
-  </div>
-)
+const App = ({ movie }) => {
+  const movie={
+    title,
+    rating,
+    length,
+  }
+  return (
+    <div className="title">
+      {title} {rating} {length}
+    </div>
+  )
+}
+
+App.propTypes = { //proptypes es una libreria(se instala con npm prototypes) sirve para evitar errores, para asegurarnos que estamos ingresando y recibiendo datos de un tipo determinado, codigo legible, para ayudar al sgte developer
+  movie: PropTypes.shape({ //shape permite validar las propiedades de un objeto
+    title: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    length: PropTypes.number.isRequired
+  })
+}
 
 render(
-  <App />,
+  <App
+    movie = {{
+      title: 'Thor: Ragnarok',
+      rating: 5,
+      length: 120
+    }}
+  />,
   document.getElementById('root')
 )
